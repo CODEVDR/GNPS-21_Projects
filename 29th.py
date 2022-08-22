e2c74b81a2cbad2bcffb4ba5de98ac7d985d4420
CODEVDR <96605996+CODEVDR@users.noreply.github.com>
2022-08-22 23:25:53 +0530

    Vdr

--- 29th.py
+++ 29th.py
@@ -1,10 +1,11 @@
 from random import *
 print("\t\t\t\tWELCOME TO CASINO GAME")
+amt=float(input(("Enter amount : ")))
 while True:
    lucky_no=randint(1,99)
    if int(input("Enter A Number : ")) ==lucky_no:
       print(f"computer choosen {lucky_no}")
-      print("You Won")
+      print(f"You Won,your reward {amt*3}")
    else:
       print(f"computer choosen {lucky_no}")
       print("You Loose")
